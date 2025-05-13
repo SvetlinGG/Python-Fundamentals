2ba11d8960769621252fde4163f5cadbf21d511d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-13 12:06:11 +0200

    number from 1 to 100

--- LAB/basic_syntax/numbers_between_one_to_100.py
+++ LAB/basic_syntax/numbers_between_one_to_100.py
@@ -3,7 +3,7 @@
 for _ in range(100):
 
     number = float(input())
-    if  1 < number < 100:
+    if  1 <= number <= 100:
         print(f'The number {number} is between 1 and 100')
         break
 
