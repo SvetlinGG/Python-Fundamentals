3c3b4fac01c51eaa01fc6150fb4a415f8ba5d4f6
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-15 11:04:59 +0200

    refactor

--- LAB/basic_syntax/word_reversed.py
+++
@@ -1,6 +0,0 @@
-words = input()
-reversed_word = ''
-
-for i in range(len(words) -1, -1, -1):
-    reversed_word += words[i]
-print(reversed_word)
--- More_exercise/sum_of_a_beach.py
+++
@@ -1,9 +0,0 @@
-text = input().lower()
-
-count = 0
-count += text.count("sand")
-count += text.count("water")
-count += text.count("fish")
-count += text.count("sun")
-
-print(count)
