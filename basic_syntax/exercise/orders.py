b876aeafd95430391c0a9df85286909648ce86c4
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-17 00:11:38 +0200

    string pureness

--- basic_syntax/exercise/orders.py
+++ basic_syntax/exercise/orders.py
@@ -8,8 +8,10 @@ for _ in range(orders):
     price_per_capsules = float(input())
     days = int(input())
     caps_per_day = int(input())
-    if 0.01 <= price_per_capsules <= 100 and 1 <= days <= 31 and 1 <= price_per_capsules <= 2000:
+    if  price_per_capsules >= 0.01 or price_per_capsules <= 100 and days >= 1 or days <= 31 and caps_per_day >= 1 or caps_per_day <= 2000:
         price = price_per_capsules * days * caps_per_day
+        if price <= 0:
+            continue
         total_price += price
         print(f'The price for the coffee is: ${price:.2f}')
 
---
+++ basic_syntax/exercise/string_pureness.py
@@ -0,0 +1,5 @@
+num_of_strings = int(input())
+
+for _ in range(num_of_strings):
+    words = input().split(' ')
+    print(words)
