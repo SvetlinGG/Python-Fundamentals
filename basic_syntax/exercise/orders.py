31a349c4676bfd99ffbe0553790b373c7d14d7ae
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-16 23:55:17 +0200

    orders finall

--- basic_syntax/exercise/orders.py
+++ basic_syntax/exercise/orders.py
@@ -8,9 +8,9 @@ for _ in range(orders):
     price_per_capsules = float(input())
     days = int(input())
     caps_per_day = int(input())
-    if price_per_capsules >= 0.01 or price_per_capsules < 100 and days >=1 or days <= 31 and price_per_capsules >= 1 or price_per_capsules < 2000:
+    if 0.01 <= price_per_capsules <= 100 and 1 <= days <= 31 and 1 <= price_per_capsules <= 2000:
         price = price_per_capsules * days * caps_per_day
         total_price += price
-        print(f'The price for coffee is: ${price:.2f}')
+        print(f'The price for the coffee is: ${price:.2f}')
 
 print(f'Total: ${total_price:.2f}')
