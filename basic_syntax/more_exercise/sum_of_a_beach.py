fb0132cf8e1204d22d45712a7609105dd9e84f58
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-18 19:29:11 +0200

    sum of beach

--- basic_syntax/more_exercise/sum_of_a_beach.py
+++ basic_syntax/more_exercise/sum_of_a_beach.py
@@ -1,30 +1,12 @@
-# text = input().lower()
-#
-# count = 0
-# count += text.count("sand")
-# count += text.count("water")
-# count += text.count("fish")
-# count += text.count("sun")
-#
-# print(count)
+text = input().lower()
 
-# budget = int(input())
+count = 0
+count += text.count("sand")
+count += text.count("water")
+count += text.count("fish")
+count += text.count("sun")
 
-total_price = 0
-all_i_need = False
-while True:
-   command = input()
-   if command == "End":
-       all_i_need = True
-       break
-   total_price += int(command)
-   if budget == total_price:
-       all_i_need = True
-       break
+print(count)
 
-   if budget < total_price:
-       print("You went in overdraft!")
-       break
 
-if all_i_need is True:
-   print("You bought everything needed.")
+
