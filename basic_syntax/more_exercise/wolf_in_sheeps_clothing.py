7394fa1a8c35b34d069be474d586c7f74abc0c82
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-21 22:40:30 +0200

    finall wolf

--- basic_syntax/more_exercise/wolf_in_sheeps_clothing.py
+++ basic_syntax/more_exercise/wolf_in_sheeps_clothing.py
@@ -1,8 +1,7 @@
 animals = input().split(', ')
-for i in range(len(animals) - 1):
-    
-    next_one = animals[i + 1]
-    if next_one == 'wolf':
-        print('Please go away and stop eating my sheep')
-    else:
-        print(f'Oi! Sheep number {i + 1}! You are about to be eaten by a wolf!')
+animals.reverse()
+
+if animals[0] == 'wolf':
+    print('Please go away and stop eating my sheep')
+else:
+    print(f'Oi! Sheep number {animals.index("wolf")}! You are about to be eaten by a wolf!')
