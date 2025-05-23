aeb3cf8bb41dbdea0a0975a15fa3a533074989fe
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-23 21:41:34 +0200

    update

--- data_types_and_variables/LAB/next_happy_year.py
+++ data_types_and_variables/LAB/next_happy_year.py
@@ -8,10 +8,10 @@ for dig in range(start_year, end_year + 1):
     uniq_digit4 = int(str(dig)[3])
     if uniq_digit1 != uniq_digit2 and uniq_digit1 != uniq_digit3 and uniq_digit1 != uniq_digit4:
         new_year.append(uniq_digit1)
-    if uniq_digit2 != uniq_digit1 and uniq_digit2 != uniq_digit3 and uniq_digit2 != uniq_digit4:
+    elif uniq_digit2 != uniq_digit1 and uniq_digit2 != uniq_digit3 and uniq_digit2 != uniq_digit4:
         new_year.append(uniq_digit2)
-    if uniq_digit3 != uniq_digit2 and uniq_digit3 != uniq_digit1 and uniq_digit3 != uniq_digit4:
+    elif uniq_digit3 != uniq_digit2 and uniq_digit3 != uniq_digit1 and uniq_digit3 != uniq_digit4:
         new_year.append(uniq_digit3)
-    if uniq_digit4 != uniq_digit1 and uniq_digit4 != uniq_digit2 and uniq_digit4 != uniq_digit3:
+    elif uniq_digit4 != uniq_digit1 and uniq_digit4 != uniq_digit2 and uniq_digit4 != uniq_digit3:
         new_year.append(uniq_digit4)
     print(new_year)
