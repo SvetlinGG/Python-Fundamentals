9019b3f608f13cafaf506eca09d32c7fc30f62c4
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-24 09:11:47 +0200

    triple latin letters

--- data_types_and_variables/exercise/triples_of_latin_letters.py
+++ data_types_and_variables/exercise/triples_of_latin_letters.py
@@ -1,6 +1,11 @@
+
 num = int(input())
 
-for i in range(0, num):
-    for j in range(0, num):
-        for k in range(0, num):
-            print(chr(num + i) + chr(num + j) + chr(num + k))
+letter_lower = 97
+
+for i in range(letter_lower, letter_lower + num):
+    for j in range(letter_lower, letter_lower + num):
+        for k in range(letter_lower, letter_lower + num):
+            print(f'{chr(i)}{chr(j)}{chr(k)}')
+
+
