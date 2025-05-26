2c7dd5789dd5ac72ba02cc333d9caac0d5e1cb61
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-26 10:52:37 +0200

    water overflow - again

--- data_types_and_variables/exercise/water_overflow.py
+++ data_types_and_variables/exercise/water_overflow.py
@@ -1,16 +1,42 @@
-number_of_lines = int(input())
+num_of_lines = int(input())
 capacity = 255
 fill = 0
 
-for _ in range(number_of_lines):
+for _ in range(num_of_lines):
     liters = int(input())
 
     if capacity - liters >= 0:
         capacity -= liters
         fill += liters
-
     else:
-        print("Insufficient capacity!")
-        print(f'{fill}')
+        print('Insufficient capacity!')
+print(f'{fill}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# number_of_lines = int(input())
+# capacity = 255
+# fill = 0
+#
+# for _ in range(number_of_lines):
+#     liters = int(input())
+#
+#     if capacity - liters >= 0:
+#         capacity -= liters
+#         fill += liters
+#
+#     else:
+#         print("Insufficient capacity!")
+# print(f'{fill}')
 
 
