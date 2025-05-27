f14af3d7b7071c0509755fe75b9ef49cd26f8934
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-27 23:41:51 +0200

    prime checker

--- data_types_and_variables/more_exercises/prime_number_checker.py
+++ data_types_and_variables/more_exercises/prime_number_checker.py
@@ -1,6 +1,6 @@
 prime_num = int(input())
 
-if prime_num > 0 and prime_num % prime_num == 0 and prime_num % 1 == 0 and prime_num % 2 != 0 and prime_num % 2 == 1:
+if prime_num > 1 and prime_num % prime_num == 0 and prime_num % 1 == 0 and prime_num < 10 and prime_num % 2 != 0:
     print('True')
 else:
     print('False')
