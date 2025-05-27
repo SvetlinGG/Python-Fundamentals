ee6dbf3a1164133b2351f65a4b959100476d390c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-27 23:14:14 +0200

    exchange integers

--- data_types_and_variables/exercise/party_profit.py
+++ data_types_and_variables/exercise/party_profit.py
@@ -1,6 +1,7 @@
 group_size = int(input())
 days = int(input())
 coins = 0
+new_people = 0
 
 for day in range(1, days + 1):
     coins += (50 - 2)
---
+++ data_types_and_variables/more_exercises/exchange_integers.py
@@ -0,0 +1,8 @@
+first_num = int(input())
+second_num = int(input())
+
+third_num = first_num
+
+if first_num < second_num:
+    
+
