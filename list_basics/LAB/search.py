53d1e67aef30638c18a14a9477cf01490a6e2e3a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-29 15:07:06 +0200

    search

---
+++ list_basics/LAB/search.py
@@ -0,0 +1,12 @@
+num = int(input())
+word = input()
+arr = []
+newArr = []
+
+for _ in range(num):
+    sentence = input()
+    arr.append(sentence)
+print(arr)
+for i in range(len(arr)):
+    for j in range(len(arr)):
+     print(j)
