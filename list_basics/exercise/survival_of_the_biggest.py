67fd3c44ba627d6e2739683822a1812c3b4a5789
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-04 22:10:12 +0200

    contenue survavul

--- list_basics/exercise/survival_of_the_biggest.py
+++ list_basics/exercise/survival_of_the_biggest.py
@@ -2,8 +2,17 @@ integers = input().split()
 count = int(input())
 current_count = 0
 arr = []
+newArr = []
 for n in integers:
     numbers = int(n)
     arr.append(numbers)
-for num in arr:
-    
+for i in arr:
+    if arr[i] < arr[i+1]:
+        current_count += 1
+        arr.pop(i)
+        if current_count == count:
+            break
+print(arr)
+
+
+
