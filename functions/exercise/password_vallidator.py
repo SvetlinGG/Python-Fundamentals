57c7632a1795a7bd60d41efae0d2dacd4cea8e75
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-06 15:56:40 +0200

    first version

---
+++ functions/exercise/password_vallidator.py
@@ -0,0 +1,18 @@
+password = input()
+length = len(password)
+
+def password_validator(password):
+    arr = []
+    for char in password:
+        arr.append(char)
+        text = arr[-2:]
+    if 6 >= len(arr) <= 10:
+        print('Password must be between 6 and 10 characters')
+    if char[-2:].isdigit():
+        print('Password must consist only of letters and digits')
+    else:
+        print('Password must have at least 2 digits')
+
+password_validator(password)
+
+
