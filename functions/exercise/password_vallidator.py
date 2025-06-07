1933462d6bb8b99dee55320f9860334b0f9e0b6c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-07 09:39:36 +0200

    perfect number

--- functions/exercise/password_vallidator.py
+++ functions/exercise/password_vallidator.py
@@ -9,9 +9,9 @@ def password_validator(password):
     if 6 >= len(arr) <= 10:
         print('Password must be between 6 and 10 characters')
     if char[-2:].isdigit():
-        print('Password must consist only of letters and digits')
-    else:
         print('Password must have at least 2 digits')
+    if char.isalnum():
+        print('Password must consist only of letters and digits')
 
 password_validator(password)
 
