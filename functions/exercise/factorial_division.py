1fee79e29b1dfb2b96060b8a44d37b5f8c93c771
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-07 13:16:15 +0200

    factorial devision

---
+++ functions/exercise/factorial_division.py
@@ -0,0 +1,12 @@
+number_one = int(input())
+number_two = int(input())
+
+def fibonacci(num_one: int, num_two: int):
+    sum = 1
+    for i in range(num_one, 0, -1):
+        sum *= i
+    total = sum / num_two
+    print(f'{total:.2f}')
+
+
+fibonacci(number_one, number_two)
