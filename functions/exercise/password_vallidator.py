800b3365df27ba93605d797cc05532bad3201653
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-07 14:52:13 +0200

    password validators - update

--- functions/exercise/password_vallidator.py
+++ functions/exercise/password_vallidator.py
@@ -1,18 +1,37 @@
+import string
+
 password = input()
 length = len(password)
 
 def password_validator(password):
-    arr = []
-    for char in password:
-        arr.append(char)
-        text = arr[-2:]
-    if 6 >= len(arr) <= 10:
+
+    allowed = string.ascii_letters + string.digits + " "
+    if 6 >= length <= 10:
         print('Password must be between 6 and 10 characters')
-    if char[-2:].isdigit():
+    if password[-2:].isalnum():
         print('Password must have at least 2 digits')
-    if char.isalnum():
-        print('Password must consist only of letters and digits')
+    for char in password:
+        if char not in allowed:
+            print('Password must consist only of letters and digits')
+
 
 password_validator(password)
+
+# password = input()
+# length = len(password)
+#
+# def password_validator(password):
+#     arr = []
+#     for char in password:
+#         arr.append(char)
+#         text = arr[-2:]
+#     if 6 >= len(arr) <= 10:
+#         print('Password must be between 6 and 10 characters')
+#     if char[-2:].isdigit():
+#         print('Password must have at least 2 digits')
+#     if char.isalnum():
+#         print('Password must consist only of letters and digits')
+#
+# password_validator(password)
 
 
