1fd231f54dfe4d6b5e06056df8f58487aebb4a65
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-09 10:48:32 +0200

    longer line

---
+++ functions/more_exercises/longer_line.py
@@ -0,0 +1,31 @@
+import math
+
+x1 = int(input())
+y1 = int(input())
+
+x2 = int(input())
+y2 = int(input())
+
+x3 = int(input())
+y3 = int(input())
+
+x4 = int(input())
+y4 = int(input())
+
+# point_one = (x1, y1)
+# point_two = (x2, y2)
+# point_three = (x3, y3)
+# point_four = (x4, y4)
+
+def longer_line(x1, y1, x2, y2, x3, y3, x4, y4):
+
+    one = (y2 - y1) / (x2 - x1)
+    line_one = y1 - one * x1
+    two = (y4 - y3) / (x4 - x3)
+    line_two = y3 - two * x3
+    if line_two > line_one:
+        print(f'({x2}, {y2})({x1}, {y1})')
+    else:
+        print(f'({x4}, {y4})({x3}, {y3})')
+
+longer_line(x1, y1, x2, y2, x3, y3, x4, y4)
