066fe773bfd509e82d25cd46623909982a8c8fb8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-12 23:32:42 +0200

    logical decision

--- lists_advanced/LAB/trains.py
+++ lists_advanced/LAB/trains.py
@@ -1,17 +1,19 @@
 train_wagons = int(input())
-train = [0, 0, 0]
+train = [0] * train_wagons
 arr = []
 
-for _ in range(train_wagons):
+while True:
     command = input()
-    arr.append(command)
-for operation in arr:
-    operation = operation.split(' ')
-    if operation[0] == 'add':
-        train[-1] = int(operation[1])
-    if operation[0] == 'insert':
-        train.pop(int(operation[1]))
-        train.insert(int(operation[1]), int(operation[2]))
+    if command == 'End':
+        break
+    for operation in arr:
+        arr.append(command)
+        operation = operation.split(' ')
+        if operation[0] == 'add':
+            train[-1] = int(operation[1])
+        if operation[0] == 'insert':
+            train.pop(int(operation[1]))
+            train.insert(int(operation[1]), int(operation[2]))
 print(train)
 
 
