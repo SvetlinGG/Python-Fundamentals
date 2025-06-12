be07bf9cc41eb2b6f4d3160cde3bbbb6ef387552
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-12 23:21:24 +0200

    more strings

--- lists_advanced/LAB/trains.py
+++ lists_advanced/LAB/trains.py
@@ -10,7 +10,7 @@ for operation in arr:
     if operation[0] == 'add':
         train[-1] = int(operation[1])
     if operation[0] == 'insert':
-        train.pop(0)
+        train.pop(int(operation[1]))
         train.insert(int(operation[1]), int(operation[2]))
 print(train)
 
