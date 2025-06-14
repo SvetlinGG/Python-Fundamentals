899602f3422bb7975348e07b3d74c072183b2221
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-14 23:13:46 +0200

    train again

--- lists_advanced/LAB/trains.py
+++ lists_advanced/LAB/trains.py
@@ -1,20 +1,16 @@
 train_wagons = int(input())
 train = [0] * train_wagons
 arr = []
-
-while True:
-    command = input()
-    if command == 'End':
-        break
-    for operation in arr:
-        arr.append(command)
+command = input()
+for operation in command:
         operation = operation.split(' ')
+        print(operation)
         if operation[0] == 'add':
             train[-1] = int(operation[1])
         if operation[0] == 'insert':
             train.pop(int(operation[1]))
             train.insert(int(operation[1]), int(operation[2]))
 print(train)
-print(command)
+
 
 
