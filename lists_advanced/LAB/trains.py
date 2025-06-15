ba458a9cc154332f8900b4a8473963ba5a1fac1b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-15 21:53:19 +0200

    train again 1

--- lists_advanced/LAB/trains.py
+++ lists_advanced/LAB/trains.py
@@ -1,6 +1,11 @@
 train_wagons = int(input())
 train = [0] * train_wagons
+for _ in range(train_wagons):
+    command = input().split(' ')
+    if command[0] == 'add':
+        train[-1] += int(command[1])
+    if command[0] == 'insert':
+        train.insert(train[0], int(command[2]))
 print(train)
-
 
 
