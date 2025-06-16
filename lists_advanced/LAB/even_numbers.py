595b0a085fe1e24a859f386d98e5e503ac810812
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-16 09:16:09 +0200

    even number 2-nd decision

--- lists_advanced/LAB/even_numbers.py
+++ lists_advanced/LAB/even_numbers.py
@@ -1,10 +1,17 @@
+main_list = [int(n) for n in input().split(", ")]
+even_list = list()
 
+for n, index in enumerate(main_list):
+    if index % 2 == 0:
+        even_list.append(n)
 
-numbers = input().split(', ')
-arr = []
-for number in numbers:
+print(even_list)
 
-    if int(number) % 2 == 0:
-        index = numbers.index(number)
-        arr.append(index)
-print(arr)
+# numbers = input().split(', ')
+# arr = []
+# for number in numbers:
+#
+#     if int(number) % 2 == 0:
+#         index = numbers.index(number)
+#         arr.append(index)
+# print(arr)
