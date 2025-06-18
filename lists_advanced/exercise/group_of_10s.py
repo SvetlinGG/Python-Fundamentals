a94ee638faeba40b46b3096f3fca749c4c61a1c2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-18 23:44:33 +0200

    group of 10's

---
+++ lists_advanced/exercise/group_of_10s.py
@@ -0,0 +1,19 @@
+numbers = [ int(num) for num  in input().split(', ')]
+arr_10 = []
+arr_20 = []
+arr_30 = []
+arr_40 = []
+arr_50 = []
+
+for num in numbers:
+
+    if num <= 10:
+        arr_10.append(num)
+    elif num <= 20:
+        arr_20.append(num)
+    elif num <= 30:
+        arr_30.append(num)
+    elif num <= 40:
+        arr_40.append(num)
+    else:
+        arr_50.append(num)
