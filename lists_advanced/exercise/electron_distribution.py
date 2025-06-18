922bbdb53eefac82d512225cc39aa070e6791bc7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-18 23:35:22 +0200

    electron distribution

---
+++ lists_advanced/exercise/electron_distribution.py
@@ -0,0 +1,28 @@
+
+
+# count_electrons = int(input())
+# electron_list = [1, 2, 3, 4]
+# electron_distribution = 0
+# arr = []
+# newArr = []
+# total = 0
+# for num in electron_list:
+#     electron_distribution = 2 * (num ** 2)
+#     if electron_distribution > 18:
+#         electron_distribution = num ** 2
+#
+#     arr.append(electron_distribution)
+#
+# for suma  in arr:
+#     total += suma
+#     newArr.append(suma)
+#     if total == count_electrons:
+#         break
+#
+#
+# print(newArr)
+
+
+
+
+
