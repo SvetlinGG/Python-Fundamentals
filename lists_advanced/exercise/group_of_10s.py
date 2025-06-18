cae4eaf81f15c2c1dec56d2db3d3ea703c2cdd82
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-19 00:34:23 +0200

    10's edited finaly

--- lists_advanced/exercise/group_of_10s.py
+++ lists_advanced/exercise/group_of_10s.py
@@ -1,25 +1,34 @@
-numbers = [ int(num) for num  in input().split(', ')]
-arr_10 = []
-arr_20 = []
-arr_30 = []
-arr_40 = []
-arr_50 = []
+number_list = [int(n) for n in input().split(", ")]
 
-for num in numbers:
+for n in range(1, 11):
+    check_list = list()
+    if len(number_list) != 0:
+        [check_list.append(i) for i in number_list if i <= (n * 10)]
+        [number_list.remove(o) for o in check_list]
+        print(f"Group of {n * 10}'s: {check_list}")
 
-    if num <= 10:
-        arr_10.append(num)
-    elif num <= 20:
-        arr_20.append(num)
-    elif num <= 30:
-        arr_30.append(num)
-    elif num <= 40:
-        arr_40.append(num)
-    else:
-        arr_50.append(num)
-
-print(f"Group of {1 * 10}'s: {arr_10}")
-print(f"Group of {2 * 10}'s: {arr_20}")
-print(f"Group of {3 * 10}'s: {arr_30}")
-print(f"Group of {4 * 10}'s: {arr_40}")
-print(f"Group of {5 * 10}'s: {arr_50}")
+# numbers = [ int(num) for num  in input().split(', ')]
+# arr_10 = []
+# arr_20 = []
+# arr_30 = []
+# arr_40 = []
+# arr_50 = []
+#
+# for num in numbers:
+#
+#     if num <= 10:
+#         arr_10.append(num)
+#     elif num <= 20:
+#         arr_20.append(num)
+#     elif num <= 30:
+#         arr_30.append(num)
+#     elif num <= 40:
+#         arr_40.append(num)
+#     elif num <= 50:
+#         arr_50.append(num)
+#
+# print(f"Group of {1 * 10}'s: {arr_10}")
+# print(f"Group of {2 * 10}'s: {arr_20}")
+# print(f"Group of {3 * 10}'s: {arr_30}")
+# print(f"Group of {4 * 10}'s: {arr_40}")
+# print(f"Group of {5 * 10}'s: {arr_50}")
