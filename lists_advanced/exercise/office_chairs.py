7cc20156267c8d9afd7f7482f3114a2cab684f4a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-18 22:34:34 +0200

    office chairs edited

--- lists_advanced/exercise/office_chairs.py
+++ lists_advanced/exercise/office_chairs.py
@@ -1,16 +1,44 @@
 rooms = int(input())
-total_chairs = 0
 free_chairs = 0
-total_person = 0
+needed_chairs = 0
+
+for i in range(1, rooms + 1):
+    chair_str, person_str = input().split()
+    chairs = len(chair_str)
+    people = int(person_str)
+
+    if chairs >= people:
+        free_chairs += chairs - people
+    else:
+        shortage = people - chairs
+        needed_chairs += shortage
+        print(f"{shortage} more chairs needed in room {i}")
+
+if needed_chairs == 0:
+    print(f"Game On, {free_chairs} free chairs left")
 
 
-for _ in range(rooms):
+# rooms = int(input())
+# total_chairs = 0
+# free_chairs = 0
+# total_person = 0
+# needed_chairs = 0
+#
+#
+# for i in range(1, rooms + 1):
+#
+#     chair, person = input().split()
+#     total_chairs += len(chair)
+#     total_person += int(person)
+#     if total_chairs >= total_person:
+#         free_chairs = total_chairs - total_person
+#     else:
+#         shortage = total_person - total_chairs
+#         needed_chairs += shortage
+#         print(f"{shortage} more chairs needed in room {i}")
+#
+# if needed_chairs == 0:
+#     print(f"Game On, {free_chairs} free chairs left")
 
-    chair, person = input().split(' ')
-    total_chairs += len(chair)
-    total_person += int(person)
-    if total_chairs >= total_person:
-        free_chairs = total_chairs - total_person
-print(f'Game On, {free_chairs} free chairs left')
 
 
