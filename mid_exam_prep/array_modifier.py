06dbf1b5d591844396c7784f69b45c5a37b25fa6
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-20 01:02:57 +0200

    array modifier

--- mid_exam_prep/array_modifier.py
+++ mid_exam_prep/array_modifier.py
@@ -1,4 +1,4 @@
-array_elements = [int(num) for num in input().split(' ')]
+array_elements = [int(num) for num in input().split()]
 
 while True:
     parts = input().split()
@@ -6,4 +6,13 @@ while True:
 
     if command == 'end':
         break
-    print(command)
+    if command == 'swap':
+        array_elements[int(parts[1])], array_elements[int(parts[2])] = array_elements[int(parts[2])], array_elements[int(parts[1])]
+    elif command == 'multiply':
+        multiplication = array_elements[int(parts[1])] * array_elements[int(parts[2])]
+        array_elements[int(parts[1])] = multiplication
+    elif command == 'decrease':
+        for i in range(len(array_elements)):
+            array_elements[i] -= 1
+
+print(", ".join(map(str, array_elements)))
