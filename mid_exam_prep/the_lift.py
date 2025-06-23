0cbed813035a8deb637fa67e707bfabcedc1b3f1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-23 23:02:48 +0200

    updated

--- mid_exam_prep/the_lift.py
+++ mid_exam_prep/the_lift.py
@@ -10,29 +10,4 @@ for i in range(len(lift_places)):
 # jump_position = 0
 #
 #
-# def jump_neighborhood(length_d):
-#     global jump_position
-#     jump_position += length_d
-#     if jump_position >= len(neighborhood):
-#         jump_position = 0
-#     if neighborhood[jump_position] == 0:
-#         print(f"Place {jump_position} already had Valentine's day.")
-#     else:
-#         neighborhood[jump_position] -= 2
-#         if neighborhood[jump_position] == 0:
-#             print(f"Place {jump_position} has Valentine's day.")
-#
-#
-# while jump_command != "Love!":
-#     jump_command = jump_command.split()
-#     jump_neighborhood(int(jump_command[1]))
-#
-#     jump_command = input()
-#
-# print(f"Cupid's last position was {jump_position}.")
-#
-# if sum(neighborhood) == 0:
-#     print("Mission was successful.")
-# else:
-#     fail_count = neighborhood.count(0)
-#     print(f"Cupid has failed {len(neighborhood) - fail_count} places.")
+
