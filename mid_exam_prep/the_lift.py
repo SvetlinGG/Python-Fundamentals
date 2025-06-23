9e71efa8cee03b6a99689fda3c3a55cd5184d70f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-23 23:03:28 +0200

    setup

--- mid_exam_prep/the_lift.py
+++ mid_exam_prep/the_lift.py
@@ -4,8 +4,7 @@ lift_places = [ int(x) for x in input().split()]
 for i in range(len(lift_places)):
     pass
 
-# neighborhood = [int(n) for n in input().split("@")]
-#
+
 # jump_command = input()
 # jump_position = 0
 #
