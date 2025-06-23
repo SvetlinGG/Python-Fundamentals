4e4251e23b6471e39d9885dd43ad1acf587f9aca
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-23 22:52:17 +0200

    no vowels

--- lists_advanced/LAB/no-vowels.py
+++ lists_advanced/LAB/no-vowels.py
@@ -1,11 +1,21 @@
+
 word = input()
-vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+vowels = ['a', 'o', 'e', 'i', 'u', 'A','I', 'U','E', 'O']
 
-def is_vowel(word):
+result = [str(letter) for letter in word if letter not in vowels]
+print(''.join(result))
 
-    for char in word:
-        if char in vowels:
-            word = word.replace(char, '')
-    print(word)
 
-is_vowel(word)
+
+
+#word = input()
+# vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+#
+# def is_vowel(word):
+#
+#     for char in word:
+#         if char in vowels:
+#             word = word.replace(char, '')
+#     print(word)
+#
+# is_vowel(word)
