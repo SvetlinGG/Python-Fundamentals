5e6efdc367257673391291872c246f3dcd03fc6d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-24 23:48:27 +0200

    trains again

--- lists_advanced/LAB/trains.py
+++ lists_advanced/LAB/trains.py
@@ -1,30 +1,34 @@
-train_wagons = int(input())
-train = [0] * train_wagons
-command = input()
 
 
-def add_people(people):
-    train[-1] += people
-
-def insert_people(wagon, number_people):
-    train[wagon] += number_people
-
-def leave_people(wagon, number_people):
-    train[wagon] -= number_people
 
 
-commands = {
-    "add": add_people,
-    "insert": insert_people,
-    "leave": leave_people
-}
-
-while command != "End":
-    command, *info = [int(x) if x.isdigit() else x for x in command.split()]
-    commands[command](*info)
-    command = input()
-
-print(train)
+# train_wagons = int(input())
+# train = [0] * train_wagons
+# command = input()
+#
+#
+# def add_people(people):
+#     train[-1] += people
+#
+# def insert_people(wagon, number_people):
+#     train[wagon] += number_people
+#
+# def leave_people(wagon, number_people):
+#     train[wagon] -= number_people
+#
+#
+# commands = {
+#     "add": add_people,
+#     "insert": insert_people,
+#     "leave": leave_people
+# }
+#
+# while command != "End":
+#     command, *info = [int(x) if x.isdigit() else x for x in command.split()]
+#     commands[command](*info)
+#     command = input()
+#
+# print(train)
 
 # train_wagons = int(input())
 # train = [0] * train_wagons
