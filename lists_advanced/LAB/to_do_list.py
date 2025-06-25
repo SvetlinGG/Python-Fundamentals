659ea7c060875fc3b341a911157565dfb8b88ac1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-25 23:04:55 +0200

    to do list

--- lists_advanced/LAB/to_do_list.py
+++ lists_advanced/LAB/to_do_list.py
@@ -1,16 +1,31 @@
-notes = [0] * 10
 
-while True:
-    command = input()
-    if command == "End":
-        break
-    tokens = command.split('-')
-    priority = int(tokens[0]) - 1
-    note = tokens[1]
-    notes.pop(priority)
-    notes.insert(priority, note)
-result = [element for element in notes if element != 0]
-print(result)
+notes = [0]
+
+
+for _ in notes:
+
+    command = input().split('-')
+
+    for num, token in enumerate(command):
+        filtered = list(filter(lambda num: num == num, command))
+        print(' '.join(filtered))
+
+
+
+
+# notes = [0] * 10
+#
+# while True:
+#     command = input()
+#     if command == "End":
+#         break
+#     tokens = command.split('-')
+#     priority = int(tokens[0]) - 1
+#     note = tokens[1]
+#     notes.pop(priority)
+#     notes.insert(priority, note)
+# result = [element for element in notes if element != 0]
+# print(result)
 
 
 
