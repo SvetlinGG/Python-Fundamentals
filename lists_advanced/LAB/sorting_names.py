fc99c1fac8b79a150b9dd5eae131fb0953d580af
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-26 15:52:37 +0200

    comment

--- lists_advanced/LAB/sorting_names.py
+++ lists_advanced/LAB/sorting_names.py
@@ -1,3 +1,5 @@
-names = input().split(', ')
-sorted_names = sorted(names, key=lambda name: (-len(name), name))
-print(sorted_names)
+
+
+# names = input().split(', ')
+# sorted_names = sorted(names, key=lambda name: (-len(name), name))
+# print(sorted_names)
