4cf71fd001366e36f5418007483123a49c5fbf6d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-26 15:36:21 +0200

    palindrome strings

--- lists_advanced/LAB/palindrome_string.py
+++ lists_advanced/LAB/palindrome_string.py
@@ -1,4 +1,16 @@
+words = [ str(w) for w  in input().split()]
+current_word = input()
+count = 0
+arr = []
 
+for word in words:
+
+    if word == word[::-1]:
+        arr.append(word)
+    if word == current_word:
+        count += 1
+print(arr)
+print(f'Found palindrome {count} times')
 
 
 # words = input().split(' ')
