99107d1661086e66b6eddf9685961d24d1763a81
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-26 21:10:03 +0200

    even numbers

--- lists_advanced/LAB/even_numbers.py
+++ lists_advanced/LAB/even_numbers.py
@@ -1,17 +1,17 @@
-main_list = [int(n) for n in input().split(", ")]
-even_list = list()
-
-for n, index in enumerate(main_list):
-    if index % 2 == 0:
-        even_list.append(n)
-
-print(even_list)
-
-# numbers = input().split(', ')
-# arr = []
-# for number in numbers:
+# main_list = [int(n) for n in input().split(", ")]
+# even_list = list()
 #
-#     if int(number) % 2 == 0:
-#         index = numbers.index(number)
-#         arr.append(index)
-# print(arr)
+# for n, index in enumerate(main_list):
+#     if index % 2 == 0:
+#         even_list.append(n)
+#
+# print(even_list)
+
+numbers = input().split(', ')
+arr = []
+for number in numbers:
+
+    if int(number) % 2 == 0:
+        index = numbers.index(number)
+        arr.append(index)
+print(arr)
