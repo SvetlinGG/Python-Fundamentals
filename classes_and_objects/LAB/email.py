f73e3165054510dd23e469997c4b7f0d5c84aa84
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-27 23:34:48 +0200

    email

--- classes_and_objects/LAB/email.py
+++ classes_and_objects/LAB/email.py
@@ -9,3 +9,22 @@ class Email:
     def send(self):
         self.is_sent = True
 
+    def get_info(self):
+        return f'{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}'
+
+emails = []
+line = input()
+while line != 'Stop':
+    info = line.split(' ')
+    sender = info[0]
+    receiver = info[1]
+    content = info[2]
+    email = Email(sender, receiver, content)
+    emails.append(email)
+    line = input()
+
+send_email = [int(x) for x in input().split(', ')]
+for x in send_email:
+    emails[x].send()
+for email in emails:
+    print(email.get_info())
