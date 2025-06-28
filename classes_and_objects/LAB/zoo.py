b96074d62c55b56d7775a6ca057bc33369c9680f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-28 11:53:49 +0200

    zoo

---
+++ classes_and_objects/LAB/zoo.py
@@ -0,0 +1,17 @@
+
+class Zoo:
+    _animals = 0
+
+    def __init__(self, name):
+        self.name = name
+        self.mammals = []
+        self.fishes = []
+        self.birds = []
+
+    def add_animals(self, species, name):
+        pass
+
+    def get_info(self, species):
+        pass
+name = input()
+number = int(input())
