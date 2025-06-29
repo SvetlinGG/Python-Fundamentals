207a9036fcd50b6c95b5e233bd84831717ed0c8b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-29 14:37:21 +0200

    posts code

--- classes_and_objects/LAB/practice_two.py
+++ classes_and_objects/LAB/practice_two.py
@@ -18,7 +18,7 @@ class User:
     def view_post(self):
         print(f"\n{self.username}'s Post: ")
         for post in self.posts:
-            print(f' - {post.content} by {post.author}')
+            print(f' - {post.content} by ({post.author.username})')
 
 
 class Post:
@@ -26,3 +26,32 @@ class Post:
     def __init__(self, author, content):
         self.author = author
         self.content = content
+
+def important(func):
+    def wrapper(*args, **kwargs):
+        content = func(*args, **kwargs)
+        return '[IMPORTANT] ' + content
+    return wrapper
+
+class AdminUser(User):
+
+    @important
+    def prepare_important_content(self, content):
+        return content
+    def create_important_content(self, content):
+        important_content = self.prepare_important_content(content)
+        post = Post(self, important_content)
+        self.posts.append(post)
+        print(f'{self.username} ;posted in IMPORTANT message: {important_content}')
+
+svetlin = User('Svetlin')
+mario = AdminUser('Mario')
+
+svetlin.add_friends(mario)
+mario.create_post('Hello tonight')
+mario.create_important_content('Hello to you tonight')
+svetlin.create_post('Hello SoftUni!')
+
+svetlin.view_post()
+mario.view_post()
+
