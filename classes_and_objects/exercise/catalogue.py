480608c76dfe9ffaadc7b494d02fbcf05042c251
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-01 00:11:06 +0200

    catalogue

---
+++ classes_and_objects/exercise/catalogue.py
@@ -0,0 +1,13 @@
+
+
+class Catalogue:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter):
+        return 
