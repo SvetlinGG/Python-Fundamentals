ea9cb55b7476e480f73a0b87082a1cd595c4f5ab
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-30 22:05:30 +0200

    storage  setup

---
+++ classes_and_objects/exercise/storage.py
@@ -0,0 +1,18 @@
+
+
+
+class Storage:
+
+    storage = []
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def add_products(self, product: str):
+        pass
+
+    def get_products(self):
+        return storage
+
+
+
