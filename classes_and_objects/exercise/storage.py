25322cf9d2c6644a3a2765ccf69fdd091d724a98
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-06-30 23:24:46 +0200

    storage setup again

--- classes_and_objects/exercise/storage.py
+++ classes_and_objects/exercise/storage.py
@@ -8,11 +8,24 @@ class Storage:
     def __init__(self, capacity):
         self.capacity = capacity
 
-    def add_products(self, product: str):
-        pass
+    def add_product(self, product: str):
+        if self.capacity > 0:
+            self.capacity -= 1
+            Storage.storage.append(product)
+
 
     def get_products(self):
-        return storage
+        return Storage.storage
+
+storage = Storage(4)
+
+storage.add_product("apple")
+storage.add_product("banana")
+storage.add_product("potato")
+storage.add_product("tomato")
+storage.add_product("bread")
+print(storage.get_products())
+
 
 
 
