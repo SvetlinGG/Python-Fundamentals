7edf008a53a1324d898fef79078520a285545b3e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-02 21:46:17 +0200

    print - keys, value, items

--- dictionaries/LAB/practice.py
+++ dictionaries/LAB/practice.py
@@ -1,13 +1,29 @@
 
 
-keys = ['name', 'age', 'major']
-values = ['Ivan', 22, 'Developer']
+# keys = ['name', 'age', 'major']
+# values = ['Ivan', 22, 'Developer']
+#
+# student = {}
+#
+# for i in range(len(keys)):
+#     key = keys[i]
+#     value = values[i]
+#
+#     student[key] = value
+# print(student)
+# print(student.keys())
+# print(student.values())
+# print(student.items())
 
-student = {}
-
-for i in range(len(keys)):
-    key = keys[i]
-    value = values[i]
-
-    student[key] = value
+student = {
+    'name': 'Ivan',
+    'age': 22,
+    'major': 'Developer',
+    'language': 'Python'
+}
 print(student)
+# print(student.get('name'))
+# print(student.get('language'))
+print(student.items())
+print(student.keys())
+print(student.values())
