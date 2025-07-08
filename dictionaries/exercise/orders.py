82b757e9e5ce90acd359bf407961bc9b75e7b7e3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-08 22:28:18 +0200

    orders

--- dictionaries/exercise/orders.py
+++ dictionaries/exercise/orders.py
@@ -7,7 +7,35 @@ while True:
     if order == 'buy':
         break
     product, price, quantity = order.split()
-    total_sum = float(price) * int(quantity)
-    orders[product] = total_sum
-for products, prices in orders.items():
-    print(f'{products} -> {prices:.2f}')
+    price = float(price)
+    quantity = int(quantity)
+    if product not in orders:
+        orders[product] = [price, quantity]
+    else:
+        orders[product][1] += quantity
+        if orders[product][0] != price:
+            orders[product][0] = price
+
+for products, (prices, quantity) in orders.items():
+    print(f'{products} -> {prices * quantity:.2f}')
+
+orders = {}
+
+# while True:
+#     command = input()
+#     if command == 'buy':
+#         break
+#
+#     product, price, quantity = command.split()
+#     price = float(price)
+#     quantity = int(quantity)
+#
+#     if product not in orders:
+#         orders[product] = [price, quantity]
+#     else:
+#         orders[product][1] += quantity
+#         if orders[product][0] != price:
+#             orders[product][0] = price
+#
+# for product, (price, quantity) in orders.items():
+#     print(f'{product} -> {price * quantity:.2f}')
