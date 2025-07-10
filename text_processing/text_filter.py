5a3c8dac81539009f7063625779c72281a5477a1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-10 21:10:57 +0200

    text filter final

--- text_processing/text_filter.py
+++ text_processing/text_filter.py
@@ -4,5 +4,16 @@ text = input()
 for word in text.split(' '):
         if word == banned_words[0] or word == banned_words[1]:
             if len(word) == len(banned_words[0]) or len(word) == len(banned_words[1]):
-                text = text.replace(word, '*')
+                text = text.replace(word, '*' * len(word))
 print(text)
+
+
+# ban_list = input().split(", ")
+# text = input()
+#
+# replace_symbol = "*"
+#
+# for ban_word in ban_list:
+#     word_len = len(ban_word)
+#     text = text.replace(ban_word, f"{word_len * replace_symbol}")
+# print(text)
