92b9db128598c76be3b463ef05f8b9aac154ecde
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-11 10:56:23 +0200

    char multiplier

--- text_processing/exercise/character_multiplier.py
+++ text_processing/exercise/character_multiplier.py
@@ -1,14 +1,10 @@
 
 words = input().split()
 sum = words[0] + words[1]
-total_word_1 = 0
-total_word_2 = 0
-total_sum = 0
-
-for word_1 in words[0]:
-    total_word_1 += ord(word_1)
-
-for word_2 in words[1]:
-    total_word_2 += ord(word_2)
-total_sum = total_word_1 + total_word_2
-print(total_sum)
+total_word_1 = [ord(first) for first in words[0]]
+total_word_2 = [ord(second) for second in words[1]]
+if len(total_word_1) > len(total_word_2):
+    pass
+else:
+    total_word_2 = total_word_2[:len(total_word_2)]
+print(total_word_1[0] * total_word_2[0])
