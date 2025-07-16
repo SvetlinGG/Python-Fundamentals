a267a383a8093b75997f8b1cfdb15d00574e10fa
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-16 22:04:40 +0200

    number validation

--- regex/LAB/example_1.py
+++ regex/LAB/example_1.py
@@ -1,10 +1,15 @@
 import re
 
-phone = '0876292882'
+# phone = '0876292882'
+#
+# pattern_phone = r'^08[7-9]\d{7}'
+#
+# if re.match(pattern_phone, phone):
+#     print(f'{phone} is valid')
+# else:
+#     print(f'{phone} is not valid')
 
-pattern_phone = r'^08[7-9]\d{7}'
+text = 'My number is 12345'
 
-if re.match(pattern_phone, phone):
-    print(f'{phone} is valid')
-else:
-    print(f'{phone} is not valid')
+result = re.findall(r'\d+', text)
+print(result)
