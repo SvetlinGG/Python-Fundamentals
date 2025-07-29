9f7775c798806693d217a317d8b181285f22e121
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-30 00:37:01 +0200

    pirates update

--- final_exam_prep/pirates.py
+++ final_exam_prep/pirates.py
@@ -6,7 +6,7 @@ while command[0] != 'Sail':
     city, population, gold = command[0], int(command[1]), int(command[2])
 
     if city not in cities.keys():
-        cities[city] = {"population": population, "gold": gold}
+        cities[city] = {"population": 0, "gold": 0}
     cities[city]["population"] += population
     cities[city]["gold"] += gold
     command = input().split('||')
@@ -32,6 +32,14 @@ while command[0] != 'End':
             print(f'{gold} gold added to the city treasury. {town} now has {total_gold} gold.')
 
     command = input().split('=>')
+if cities:
+    print(f'Ahoy, Captain! There are {len(cities)} wealthy settlements to go to:')
+    for town, town_data in cities.items():
+        people = town_data['population']
+        gold = town_data['gold']
+        print(f'{town} -> Population: {people} citizens, Gold: {gold} kg')
+else:
+    print(f'Ahoy, Captain! All targets have been plundered and destroyed!')
 
 
 
