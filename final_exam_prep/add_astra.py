732d62ff4e776de638cb61bff80d8c4781127208
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-29 07:49:41 +0200

    add astra

--- final_exam_prep/add_astra.py
+++ final_exam_prep/add_astra.py
@@ -3,3 +3,7 @@ import re
 information = input()
 pattern = r'([#|])([A-Za-z\s]+)(\1)(\d{2}\/)(\d{2}\/)(\d{2})\1(\d+)\1'
 matches = re.findall(pattern, information)
+sum_calories = sum([int(calories[3]) for calories in matches])
+day_calories = sum_calories // 2000
+
+print(day_calories)
