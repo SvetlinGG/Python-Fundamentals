792e4b97c279de59d002f47ec16442a0cca0a784
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-29 13:38:31 +0200

    add astra submit

--- final_exam_prep/add_astra.py
+++ final_exam_prep/add_astra.py
@@ -1,9 +1,10 @@
 import re
 
 information = input()
-pattern = r'([#|])([A-Za-z\s]+)(\1)(\d{2}\/)(\d{2}\/)(\d{2})\1(\d+)\1'
+pattern = r"(?i)([#|])([a-z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1"
 matches = re.findall(pattern, information)
 sum_calories = sum([int(calories[3]) for calories in matches])
-day_calories = sum_calories // 2000
-
-print(day_calories)
+days = sum_calories // 2000
+print(f'You have food to last you for: {days} days!')
+for item in matches:
+    print(f'Item: {item[1]}, Best before: {item[2]}, Nutrition: {item[3]}')
