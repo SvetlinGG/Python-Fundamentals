82b6451870507cb18be186ef127a56322e7b2d3b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-29 23:28:04 +0200

    activation keys

---
+++ final_exam_prep/activation_keys.py
@@ -0,0 +1,16 @@
+
+activation_key = input()
+
+def contains(substring, activation_key):
+    if substring in activation_key:
+        return f'{activation_key} contains {substring}'
+    else:
+        return f'Substring not found!'
+def flipper(activation_key, start_index, end_index):
+
+
+
+
+
+while command != 'Generate':
+
