03582d0e09dd7769c7b91616a5144d15be7d4239
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-29 14:05:30 +0200

    finall setup astra regex

--- final_exam_prep/add_astra.py
+++ final_exam_prep/add_astra.py
@@ -1,7 +1,8 @@
 import re
 
 information = input()
-pattern = r"(?i)([#|])([a-z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1"
+#pattern = r"(?i)([#|])([a-z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1"
+pattern = r'([#|])([A-Za-z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1'
 matches = re.findall(pattern, information)
 sum_calories = sum([int(calories[3]) for calories in matches])
 days = sum_calories // 2000
