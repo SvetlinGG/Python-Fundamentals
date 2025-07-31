b8a29774de7671e30a897758cdee18a129185c84
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-31 10:41:57 +0200

    heroes code update

--- final_exam_prep/heroes_of_code_and_logic.py
+++ final_exam_prep/heroes_of_code_and_logic.py
@@ -29,6 +29,22 @@ while command != 'End':
     elif command_type == 'TakeDamage':
         damage = int(operation[2])
         attacker = operation[3]
-        
+        current_hp = heroes[hero_name]['HP']
+        current_hp -= damage
+        if heroes[hero_name]['HP'] > 0:
+            print(f'{hero_name} was hit for {damage} HP by {attacker} and now has {current_hp} HP left!"')
+        else:
+            print(f'{hero_name} has been killed by {attacker}!"')
+    elif command_type == 'CastSpell':
+        mp_needed = int(operation[2])
+        spell_name = operation[3]
+        current_mp = heroes[hero_name]['MP']
+        if current_mp > mp_needed:
+            mana_points_left = current_mp - mp_needed
+            print(f'{hero_name} has successfully cast {spell_name} and now has {mana_points_left} MP!"')
+        else:
+            print(f'{hero_name} does not have enough MP to cast {spell_name}!"')
+
+
     command = input()
 
