75ee7584f4df26120409dee58e575758379c1ac0
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-01 12:18:44 +0200

    password reset

--- final_exam_prep/password_reset.py
+++ final_exam_prep/password_reset.py
@@ -5,11 +5,24 @@ password = input()
 command = input().split(' ')
 while command[0] != 'Done':
 
-     operation = command
+        operation = command[0]
         if operation == 'TakeOdd':
-            print(password[1::2])
+            password = password[1::2]
+            print(password)
         elif operation == 'Cut':
             cur_index, length = int(command[1]), int(command[2])
-            
+            substring = password[cur_index:cur_index+length]
+            password = password.replace(substring, "", 1)
+            print(password)
+        elif operation == 'Substitute':
+            substring, substitute = command[1], command[2]
+            if substring in password:
+                password = password.replace(substring, substitute)
+                print(password)
+            else:
+                print('Nothing to replace!')
+        command = input().split(' ')
+
+print(f'Your password is: {password}')
 
 
