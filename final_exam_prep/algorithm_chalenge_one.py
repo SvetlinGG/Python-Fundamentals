2976b74672dd9414d4c141f6cc662f9cf241dbf9
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-02 13:09:33 +0200

    algorithm

---
+++ final_exam_prep/algorithm_chalenge_one.py
@@ -0,0 +1,9 @@
+
+algorith = [1, 2, 3, 1, 2, 4, 5]
+
+# output - [[1, 2, 3], [2, 4, 5]]
+#  text = """You're given a list of integers.
+# Your task is to find and return all the longest non-repeating sublists from the list
+#
+# This challenge will help you practice list slicing, tracking unique values,
+# and using loops and conditionals to maintain sequences dynamically."""
