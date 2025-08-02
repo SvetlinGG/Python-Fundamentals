eb29a8dc18c4852b0faf7b5c37e40ef5487d4daf
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-02 11:23:40 +0200

    inthe beginning

--- final_exam_prep/bakery_shop.py
+++ final_exam_prep/bakery_shop.py
@@ -1,37 +1,44 @@
 
-food = {}
-while True:
-    command = input().split(' ')
-    operation = command[0]
-
-    if operation == 'Complete':
-        break
-
-    quantity = int(command[1])
-    food_type = command[2]
-
-    if operation == 'Receive':
-        if quantity <= 0:
-            continue
-        if food_type not in food:
-            food[food_type] = 0
-        food[food_type] += quantity
-
-    elif operation == 'Sell':
-        if food_type not in food:
-            print(f'You do not have any {food_type}.')
-        elif food[food_type] < quantity:
-            print(f"There aren't enough {food_type}. You sold the last {food[food_type]}.")
-            del food[food_type]
-        else:
-            food[food_type] -= quantity
-            print(f'You sold {quantity} {food_type}.')
-            if food[food_type] == 0:
-                del food[food_type]
 
 
-for item, qty in food.items():
-    print(f'{item}: {qty}')
+
+
+
+
+
+# food = {}
+# while True:
+#     command = input().split(' ')
+#     operation = command[0]
+#
+#     if operation == 'Complete':
+#         break
+#
+#     quantity = int(command[1])
+#     food_type = command[2]
+#
+#     if operation == 'Receive':
+#         if quantity <= 0:
+#             continue
+#         if food_type not in food:
+#             food[food_type] = 0
+#         food[food_type] += quantity
+#
+#     elif operation == 'Sell':
+#         if food_type not in food:
+#             print(f'You do not have any {food_type}.')
+#         elif food[food_type] < quantity:
+#             print(f"There aren't enough {food_type}. You sold the last {food[food_type]}.")
+#             del food[food_type]
+#         else:
+#             food[food_type] -= quantity
+#             print(f'You sold {quantity} {food_type}.')
+#             if food[food_type] == 0:
+#                 del food[food_type]
+#
+#
+# for item, qty in food.items():
+#     print(f'{item}: {qty}')
 
 
 
