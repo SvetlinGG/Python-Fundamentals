036294495058de5e25ae685df314ac745a0dc613
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-02 10:50:35 +0200

    update baker shop

--- final_exam_prep/bakery_shop.py
+++ final_exam_prep/bakery_shop.py
@@ -9,18 +9,48 @@ while True:
 
     quantity = int(command[1])
     food_type = command[2]
-    food = {"food_type": food_type, "quantity": quantity}
+
     if operation == 'Receive':
-        food.update()
-        if food_type is not food['food_type']:
-            food['food_type'] = food['food_type']
-            if food[quantity] <= 0:
-                continue
+        if quantity <= 0:
+            continue
+        if food_type not in food:
+            food[food_type] = 0
+        food[food_type] += quantity
+
     elif operation == 'Sell':
-        quantity = int(command[1])
-        food_type = command[2]
-        if food_type is not food['food_type']:
+        if food_type not in food:
             print(f'You do not have any {food_type}.')
+        elif food[food_type] < quantity:
+            print(f"There aren't enough {food_type}. You sold the last {food[food_type]}.")
+            del food[food_type]
+        else:
+            food[food_type] -= quantity
+            print(f'You sold {quantity} {food_type}.')
+            if food[food_type] == 0:
+                del food[food_type]
+
+    # Показваме наличната храна
+    for item, qty in food.items():
+        print(f"{item}: {qty}")
+
+
+
+
+
+
+
+    # food = {"food_type": food_type, "quantity": quantity}
+    # if operation == 'Receive':
+    #     food.update()
+    #     if food_type != food['food_type']:
+    #         food['food_type'] = food['food_type']
+    #         if food[quantity] <= 0:
+    #             continue
+    # elif operation == 'Sell':
+    #     quantity = int(command[1])
+    #     food_type = command[2]
+    #     if food_type != food['food_type']:
+    #         print(f'You do not have any {food_type}.')
 
 
 
