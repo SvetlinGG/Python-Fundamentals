ffebab93bae05fe3e07ca9e9b00cb07d02281c6b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-02 10:16:26 +0200

    bakery shop

--- final_exam_prep/bakery_shop.py
+++ final_exam_prep/bakery_shop.py
@@ -12,6 +12,15 @@ while True:
     food = {"food_type": food_type, "quantity": quantity}
     if operation == 'Receive':
         food.update()
-        print(food)
-    #command = input().split(' ')
+        if food_type is not food['food_type']:
+            food['food_type'] = food['food_type']
+            if food[quantity] <= 0:
+                continue
+    elif operation == 'Sell':
+        quantity = int(command[1])
+        food_type = command[2]
+        if food_type is not food['food_type']:
+            print(f'You do not have any {food_type}.')
+
+
 
